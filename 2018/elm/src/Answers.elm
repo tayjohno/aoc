da7ce5de821235401e5aceee25240a3967cac4e3
Taylor Sampson <tayjohno@gmail.com>
2019-09-05 17:35:44 -0500

    Cleaning up answers page. Getting things to load async

--- 2018/elm/src/Answers.elm
+++ 2018/elm/src/Answers.elm
@@ -1,5 +1,6 @@
 module Answers exposing (main)
 
+import Basics as Never exposing (Never)
 import Browser
 import Day01
 import Day02
@@ -10,9 +11,12 @@ import Day06
 import Day07
 import Day08
 import Day09
-import Html exposing (Html, div, h2, h3, p, span, text, textarea)
+import Html exposing (Html, button, div, h2, p, span, text, textarea)
 import Html.Attributes exposing (attribute, class, style)
+import Html.Events exposing (onClick)
 import Html.Lazy exposing (lazy, lazy2, lazy3)
+import Process
+import Task exposing (Task)
 
 
 type alias Day =
@@ -30,9 +34,9 @@ type SolutionStatus
 
 
 type Msg
-    = Reset
-    | Stuff
-    | Things
+    = Updated Day
+    | CalculatePartOne Day
+    | CalculatePartTwo Day
 
 
 
@@ -43,8 +47,17 @@ type alias Model =
     { dayConfiguration : List Day }
 
 
-initialModel =
-    { dayConfiguration = initialConfiguration }
+init : () -> ( Model, Cmd Msg )
+init _ =
+    let
+        day =
+            initialConfiguration |> firstDay
+    in
+    ( { dayConfiguration = initialConfiguration }
+    , Process.sleep 100
+        |> Task.andThen (\_ -> Task.succeed (Updated { day | partOne = solve day.partOne }))
+        |> Task.perform identity
+    )
 
 
 newDay : String -> (() -> Maybe String) -> (() -> Maybe String) -> Day
@@ -56,41 +69,54 @@ newDay name partOne partTwo =
     }
 
 
+firstDay : List Day -> Day
+firstDay days =
+    days
+        |> List.head
+        |> Maybe.withDefault (newDay "December One" Day01.partOne Day01.partTwo)
+
+
+notImplemented : () -> Maybe String
+notImplemented _ =
+    Nothing
+
+
 initialConfiguration : List Day
 initialConfiguration =
-    [ newDay "December One" (\_ -> Nothing) (\_ -> Nothing) -- Day01.partOne Day01.partTwo
-    , newDay "December Two" (\_ -> Nothing) (\_ -> Nothing) -- Day02.partOne Day02.partTwo
-    , newDay "December Three" (\_ -> Nothing) (\_ -> Nothing) -- Day03.partOne Day03.partTwo
-    , newDay "December Four" (\_ -> Nothing) (\_ -> Nothing) -- Day04.partOne Day04.partTwo
-    , newDay "December Five" (\_ -> Nothing) (\_ -> Nothing) -- Day05.partOne Day05.partTwo
-    , newDay "December Six" (\_ -> Nothing) (\_ -> Nothing) -- Day06.partOne Day06.partTwo
-    , newDay "December Seven" (\_ -> Nothing) (\_ -> Nothing) -- Day07.partOne Day07.partTwo
-    , newDay "December Eight" (\_ -> Nothing) (\_ -> Nothing) -- Day08.partOne Day08.partTwo
-    , newDay "December Nine" (\_ -> Nothing) (\_ -> Nothing) -- Day09.partOne Day09.partTwo
-    , newDay "December Ten" (\_ -> Nothing) (\_ -> Nothing)
-    , newDay "December Eleven" (\_ -> Nothing) (\_ -> Nothing)
-    , newDay "December Twelve" (\_ -> Nothing) (\_ -> Nothing)
-    , newDay "December Thirteen" (\_ -> Nothing) (\_ -> Nothing)
-    , newDay "December Fourteen" (\_ -> Nothing) (\_ -> Nothing)
-    , newDay "December Fifteen" (\_ -> Nothing) (\_ -> Nothing)
-    , newDay "December Sixteen" (\_ -> Nothing) (\_ -> Nothing)
-    , newDay "December Seventeen" (\_ -> Nothing) (\_ -> Nothing)
-    , newDay "December Eighteen" (\_ -> Nothing) (\_ -> Nothing)
-    , newDay "December Nineteen" (\_ -> Nothing) (\_ -> Nothing)
-    , newDay "December Twenty" (\_ -> Nothing) (\_ -> Nothing)
-    , newDay "December Twenty-One" (\_ -> Nothing) (\_ -> Nothing)
-    , newDay "December Twenty-Two" (\_ -> Nothing) (\_ -> Nothing)
-    , newDay "December Twenty-Three" (\_ -> Nothing) (\_ -> Nothing)
-    , newDay "December Twenty-Four" (\_ -> Nothing) (\_ -> Nothing)
-    , newDay "December Twenty-Five" (\_ -> Nothing) (\_ -> Nothing)
+    [ newDay "December One" Day01.partOne Day01.partTwo
+    , newDay "December Two" Day02.partOne Day02.partTwo
+    , newDay "December Three" Day03.partOne Day03.partTwo
+    , newDay "December Four" Day04.partOne Day04.partTwo
+    , newDay "December Five" Day05.partOne Day05.partTwo
+    , newDay "December Six" Day06.partOne Day06.partTwo
+    , newDay "December Seven" Day07.partOne Day07.partTwo
+    , newDay "December Eight" Day08.partOne Day08.partTwo
+    , newDay "December Nine" notImplemented notImplemented
+    , newDay "December Ten" notImplemented notImplemented
+    , newDay "December Eleven" notImplemented notImplemented
+    , newDay "December Twelve" notImplemented notImplemented
+    , newDay "December Thirteen" notImplemented notImplemented
+    , newDay "December Fourteen" notImplemented notImplemented
+    , newDay "December Fifteen" notImplemented notImplemented
+    , newDay "December Sixteen" notImplemented notImplemented
+    , newDay "December Seventeen" notImplemented notImplemented
+    , newDay "December Eighteen" notImplemented notImplemented
+    , newDay "December Nineteen" notImplemented notImplemented
+    , newDay "December Twenty" notImplemented notImplemented
+    , newDay "December Twenty-One" notImplemented notImplemented
+    , newDay "December Twenty-Two" notImplemented notImplemented
+    , newDay "December Twenty-Three" notImplemented notImplemented
+    , newDay "December Twenty-Four" notImplemented notImplemented
+    , newDay "December Twenty-Five" notImplemented notImplemented
     ]
 
 
 main =
-    Browser.sandbox
-        { init = initialModel
+    Browser.element
+        { init = init
         , view = view
         , update = update
+        , subscriptions = subscriptions
         }
 
 
@@ -120,12 +146,122 @@ prepend a b =
 -- UPDATE
 
 
-update : Msg -> Model -> Model
-update _ model =
-    model
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Updated day ->
+            ( { model | dayConfiguration = updateDayList day model.dayConfiguration }
+            , doNextThing model day
+            )
+
+        CalculatePartOne day ->
+            case day.partOne of
+                Pending method ->
+                    ( { model | dayConfiguration = updateDayList { day | partOne = Solving method } model.dayConfiguration }
+                    , delayedStartPartOne day
+                    )
+
+                _ ->
+                    ( model, Cmd.none )
+
+        CalculatePartTwo day ->
+            case day.partTwo of
+                Pending method ->
+                    ( { model | dayConfiguration = updateDayList { day | partTwo = Solving method } model.dayConfiguration }
+                    , delayedStartPartTwo day
+                    )
+
+                _ ->
+                    ( model, Cmd.none )
 
 
-dayDiv : Day -> Html msg
+doNextThing : Model -> Day -> Cmd Msg
+doNextThing model day =
+    case ( day.partOne, day.partTwo ) of
+        ( Solved _, Solved _ ) ->
+            case getNextDay model.dayConfiguration day of
+                Nothing ->
+                    Debug.log "oops" Cmd.none
+
+                Just nextDay ->
+                    doNextThing model nextDay
+
+        ( Solved _, _ ) ->
+            Task.succeed (CalculatePartTwo day)
+                |> Task.perform identity
+
+        _ ->
+            Task.succeed (CalculatePartOne day)
+                |> Task.perform identity
+
+
+getNextDay : List Day -> Day -> Maybe Day
+getNextDay days day =
+    case days of
+        [] ->
+            Nothing
+
+        head :: tail ->
+            if head.name == day.name then
+                List.head tail
+
+            else
+                getNextDay tail day
+
+
+delayedStartPartOne : Day -> Cmd Msg
+delayedStartPartOne day =
+    Process.sleep 200
+        |> Task.andThen (\_ -> Task.succeed (Updated { day | partOne = solve day.partOne }))
+        |> Task.perform identity
+
+
+delayedStartPartTwo : Day -> Cmd Msg
+delayedStartPartTwo day =
+    Process.sleep 200
+        |> Task.andThen (\_ -> Task.succeed (Updated { day | partTwo = solve day.partTwo }))
+        |> Task.perform identity
+
+
+solve : SolutionStatus -> SolutionStatus
+solve status =
+    case status of
+        Solved _ ->
+            status
+
+        Solving method ->
+            Solved (method ())
+
+        Pending method ->
+            Solved (method ())
+
+
+{-| Takes a list of days and an updated day and replaces that day in the list with it's new value
+-}
+updateDayList : Day -> List Day -> List Day
+updateDayList day list =
+    case list of
+        [] ->
+            []
+
+        head :: tail ->
+            if day.name == head.name then
+                day :: tail
+
+            else
+                head :: updateDayList day tail
+
+
+
+-- Subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+dayDiv : Day -> Html Msg
 dayDiv day =
     div
         (case ( day.partOne, day.partTwo ) of
@@ -141,20 +277,21 @@ dayDiv day =
         [ div
             [ class "heading-wrapper" ]
             [ h2 [] [ text day.name ] ]
-        , partField "Part One: " day.partOne
-        , partField "Part Two: " day.partTwo
+        , solutionFields "Part One:" (CalculatePartOne day) day.partOne
+        , solutionFields "Part Two:" (CalculatePartTwo day) day.partTwo
         , textarea [ class "hidden-debug", attribute "readonly" "" ] [ text (Maybe.withDefault "test test test" day.debugString) ]
         ]
 
 
-partField : String -> SolutionStatus -> Html msg
-partField labelString solution =
+solutionFields : String -> Msg -> SolutionStatus -> Html Msg
+solutionFields titleString calculateMessage solutionStatus =
     p
         ([ class "param" ]
-            |> List.append (solutionAttributes solution)
+            |> List.append (solutionAttributes solutionStatus)
         )
-        [ span [ class "param-name" ] [ text labelString ]
-        , span [ class "param-value" ] [ text (solutionText solution) ]
+        [ span [ class "param-name" ] [ text titleString ]
+        , text " "
+        , span [ class "param-value" ] [ text (solutionText solutionStatus) ]
         ]
 
 
