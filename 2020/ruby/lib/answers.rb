cbade0bed61160ef29f1d7b47fd01f7f9fc27c44
Taylor Sampson <taylor.sampson@drip.com>
2020-12-03 13:03:25 -0600

    Slightly shorter day3

--- 2020/ruby/lib/answers.rb
+++ 2020/ruby/lib/answers.rb
@@ -7,4 +7,4 @@ puts "\nday02"
 puts I2.scan(/(\d+)-(\d+) (\w): (\w+)/).map{|a,b,*c|[a.to_i,b.to_i,*c]}.reduce([0]*2){|(x,y),(a,b,c,d)|[x+((a..b).cover?(d.count c)?1:0),y+((d[a-1]==c)^(d[b-1]==c)?1:0)]}
 
 puts "\nday03"
-I3.split.map{_1.split("")}.then{|m|[1,3,5,7,1].zip([1]*4<<2).map{|x,y|(0..(m.size-1)).count{|i|(i%y==0)&&(m[i][((i*x)/y)%m[i].size]=='#')}}}.tap{p _1[1],_1.reduce(:*)}
+I3.scan(/#{"(.)"*31}/).then{|m|[1,3,5,7,1].zip([1]*4<<2).map{|x,y|(0..(m.size-1)).count{|i|(i%y==0)&&(m[i][((i*x)/y)%m[i].size]=='#')}}}.tap{p _1[1],_1.reduce(:*)}
