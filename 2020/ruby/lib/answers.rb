7c947f02baba84cb40f081fc4069aa6baeaa8bf7
Taylor Sampson <taylor.sampson@drip.com>
2020-12-03 10:32:08 -0600

    Code golf

--- 2020/ruby/lib/answers.rb
+++ 2020/ruby/lib/answers.rb
@@ -1,7 +1,7 @@
 require_relative "./inputs"
 
 puts "day01"
-I1.split.map(&:to_i).tap{|i|p i.combination(2).find{_1+_2==2020}.reduce(:*)}.tap{|i|p i.combination(3).find{_1+_2+_3==2020}.reduce(:*)}
+->(n){->(i){p i.combination(n).find{_1.sum==2020}.reduce(:*)}}.tap{|e|I1.split.map(&:to_i).tap(&e[2]).tap(&e[3])}
 
 puts "\nday02"
 puts I2.scan(/(\d+)-(\d+) (\w): (\w+)/).map{|a,b,c,d|[a.to_i,b.to_i,c,d]}.reduce([0]*2){|(x,y),(a,b,c,d)|[x+((a..b).cover?(d.count c)?1:0),y+((d[a-1]==c)^(d[b-1]==c)?1:0)]}
