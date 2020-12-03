19e39e315b576ceaa1057806347fdff86637ac6a
Taylor Sampson <taylor.sampson@drip.com>
2020-12-03 12:43:28 -0600

    Shorter answers

--- 2020/ruby/lib/answers.rb
+++ 2020/ruby/lib/answers.rb
@@ -4,7 +4,7 @@ puts "day01"
 ->(n){->(i){p i.combination(n).find{_1.sum==2020}.reduce(:*)}}.tap{|e|I1.split.map(&:to_i).tap(&e[2]).tap(&e[3])}
 
 puts "\nday02"
-puts I2.scan(/(\d+)-(\d+) (\w): (\w+)/).map{|a,b,c,d|[a.to_i,b.to_i,c,d]}.reduce([0]*2){|(x,y),(a,b,c,d)|[x+((a..b).cover?(d.count c)?1:0),y+((d[a-1]==c)^(d[b-1]==c)?1:0)]}
+puts I2.scan(/(\d+)-(\d+) (\w): (\w+)/).map{|a,b,*c|[a.to_i,b.to_i,*c]}.reduce([0]*2){|(x,y),(a,b,c,d)|[x+((a..b).cover?(d.count c)?1:0),y+((d[a-1]==c)^(d[b-1]==c)?1:0)]}
 
 puts "\nday03"
-I3.split.map{_1.split("")}.yield_self{|m|[[1,1],[3,1],[5,1],[7,1],[1,2]].map{|x,y|(0..(m.length-1)).count{|i|(i%y==0)&&(m[i][((i*x)/y)%m[i].length]=='#')}}}.tap{p(_1[1],_1.reduce(:*))}
+I3.split.map{_1.split("")}.then{|m|[1,3,5,7,1].zip([1]*4<<2).map{|x,y|(0..(m.size-1)).count{|i|(i%y==0)&&(m[i][((i*x)/y)%m[i].size]=='#')}}}.tap{p _1[1],_1.reduce(:*)}
