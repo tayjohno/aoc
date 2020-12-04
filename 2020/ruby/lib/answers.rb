ce4261ece1c1266f5afa2c200ee7b1536ba6e86d
Taylor Sampson <taylor.sampson@drip.com>
2020-12-04 09:44:30 -0600

    Shorter with regex

--- 2020/ruby/lib/answers.rb
+++ 2020/ruby/lib/answers.rb
@@ -10,4 +10,4 @@ puts "\nday03"
 I3.split.map(&:chars).then{|m|[1,3,5,7,1].zip([1]*4<<2).map{|x,y|(0..(m.size-1)).count{|i|(i%y==0)&&(m[i][((i*x)/y)%m[i].size]=='#')}}}.tap{p _1[1],_1.reduce(:*)}
 
 puts "\nday04"
-p %w[byr iyr eyr hgt hcl ecl pid].then{|r|I4.split("\n\n").map{_1.scan(/(\S+):(\S+)/).to_h}.tap{p _1.map(&:keys).count{|l|r&l==r}}}.count{|r|(1920..2002).cover?(r["byr"].to_i)&&(2010..2020).cover?(r["iyr"].to_i)&&(2020..2030).cover?(r["eyr"].to_i)&&((r["hgt"]=~/cm/&&(150..193).cover?(r["hgt"].to_i))||(r["hgt"]=~/in/&&(59..76).cover?(r["hgt"].to_i)))&&r["hcl"]=~/^\#[\da-f]{6}$/&&%w[amb blu brn gry grn hzl oth].include?(r["ecl"])&&r["pid"]=~/^\d{9}$/}
+I4.split("\n\n").tap{|i|p i.count{|s|%w[byr iyr eyr hgt hcl ecl pid].all?{s.include?_1}}}.tap{|i|p i.count{_1.tr("\n"," ")=~/(?=.*byr:(19[2-9]\d|200[0-2]))(?=.*iyr:20(1\d|20))(?=.*eyr:20(2\d|30))(?=.*hgt:((59|6\d|7[0-6])in|(1[5-8]\d|19[0-3])cm))(?=.*hcl:\#[\da-f]{6})(?=.*ecl:(amb|blu|brn|gry|grn|hzl|oth))(?=.*pid:\d{9}(\s|\Z))/}}
