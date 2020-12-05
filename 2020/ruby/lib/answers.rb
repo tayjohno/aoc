3df1bf88545f12e0808bc51be125fa901feb2ccc
Taylor Sampson <taylor.sampson@drip.com>
2020-12-04 23:37:15 -0600

    Shortening day 4

--- 2020/ruby/lib/answers.rb
+++ 2020/ruby/lib/answers.rb
@@ -10,7 +10,7 @@ puts "\nday03"
 I3.split.map(&:chars).then{|m|[1,3,5,7,1].zip([1]*4<<2).map{|x,y|(0..(m.size-1)).count{|i|(i%y==0)&&(m[i][((i*x)/y)%m[i].size]=='#')}}}.tap{p _1[1],_1.reduce(:*)}
 
 puts "\nday04"
-I4.split("\n\n").tap{|i|p i.count{|s|%w[byr iyr eyr hgt hcl ecl pid].all?{s.include?_1}}}.tap{|i|p i.count{_1.split.sort.join=~/byr:(19[2-9]\d|200[0-2])(cid:.*)?ecl:(amb|blu|brn|gry|grn|hzl|oth)eyr:20(2\d|30)hcl:\#[\da-f]{6}hgt:((59|6\d|7[0-6])in|(1[5-8]\d|19[0-3])cm)iyr:20(1\d|20)pid:\d{9}\Z/}}
+I4.split("\n\n").tap{|i|p i.count{|s|%w[byr iyr eyr hgt hcl ecl pid].all?{s.include?_1}}}.tap{|i|p i.count{_1.split.sort.join=~/byr:(19[2-9]\d|200[0-2]).*ecl:(amb|blu|brn|gry|grn|hzl|oth)eyr:20(2\d|30)hcl:\#[\da-f]{6}hgt:((59|6\d|7[0-6])in|(1[5-8]\d|19[0-3])cm)iyr:20(1\d|20)pid:\d{9}\Z/}}
 
 puts "\nday05"
 p ((0..915).to_a-I5.split.map{_1.tr("FLBR","001").to_i(2)}.sort.tap{p _1[-1]}).last
